7dd1a9ea3bd488a0207939f62e7b1d250c7287d7
HAS <237227757@qq.com>
2025-06-08 19:46:21 +0800

    Update diy-part3.sh

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -24,21 +24,21 @@ fi
 # echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 
 # 删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
-rm -f package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
-if [ $? -eq 0 ]; then
-  echo "已删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p"
-else
-  echo "错误：删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p 失败"
-fi
+#rm -f package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
+#if [ $? -eq 0 ]; then
+#  echo "已删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p"
+#else
+#  echo "错误：删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p 失败"
+#fi
 
 # 创建 MT7981 固件符号链接
-EEPROM_FILE="package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin"
-if [ -f "$EEPROM_FILE" ]; then
-  mkdir -p files/lib/firmware
-  ln -sf /lib/firmware/MT7981_iPAiLNA_EEPROM.bin files/lib/firmware/e2p
-  echo "符号链接已创建"
-  ls -l files/lib/firmware/e2p || { echo "错误：符号链接创建失败"; exit 1; }
-else
-  echo "错误：$EEPROM_FILE 不存在，无法创建符号链接"
-  exit 1
-fi
+#EEPROM_FILE="package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin"
+#if [ -f "$EEPROM_FILE" ]; then
+#  mkdir -p files/lib/firmware
+#  ln -sf /lib/firmware/MT7981_iPAiLNA_EEPROM.bin files/lib/firmware/e2p
+#  echo "符号链接已创建"
+#  ls -l files/lib/firmware/e2p || { echo "错误：符号链接创建失败"; exit 1; }
+#else
+#  echo "错误：$EEPROM_FILE 不存在，无法创建符号链接"
+#  exit 1
+#fi
