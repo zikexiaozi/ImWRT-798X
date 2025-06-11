5efecb8dca66fcba9e39d9fc36bcfe1c82c073ad
HAS <237227757@qq.com>
2025-06-11 20:51:06 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -8,27 +8,7 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # 文件名: diy-part1.sh
 # 描述: OpenWrt DIY 脚本第一部分 (更新 feeds 之前)
-#
 
-# 替换 feeds.conf.default 为指定 URL 的 feeds.conf
-FEEDS_CONF_URL="https://mirrors.pku.edu.cn/immortalwrt/snapshots/packages/aarch64_cortex-a53/feeds.conf"
-
-# 下载新的 feeds.conf 文件
-echo "正在下载 feeds.conf 从 ${FEEDS_CONF_URL}..."
-if curl -s -o feeds.conf.default "${FEEDS_CONF_URL}"; then
-  echo "feeds.conf.default 已成功下载并替换"
-else
-  echo "错误：无法下载 feeds.conf 从 ${FEEDS_CONF_URL}"
-  exit 1
-fi
-
-# 检查 feeds.conf.default 是否存在
-if [ -f feeds.conf.default ]; then
-  echo "feeds.conf.default 已存在"
-else
-  echo "错误：feeds.conf.default 未找到"
-  exit 1
-fi
 
 # 创建自定义 distfeeds.conf 文件
 CUSTOM_DISTFEEDS_CONF="files/etc/opkg/distfeeds.conf"
