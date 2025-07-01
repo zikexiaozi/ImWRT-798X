cdca07731ddf48822a5bbea6b89415aa1ad59ac4
zikexiaozi <97644359@qq.com>
2025-07-01 11:05:09 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -26,7 +26,38 @@ echo "自定议源修改成功"
 
 # 预装 OpenClash（已注释，保持不变）
 echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+# echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+
+#删除原默认主题
+#rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-bootstrap
+#rm -rf package/lean/luci-theme-material
+#rm -rf package/lean/luci-app-privoxy
+#rm -rf package/lean/luci-theme-ifit
+
+#取消原主题luci-theme-bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改 luci-theme-netgear 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+#sed -i 's/luci-theme-bootstrap/luci-theme-ifit/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' ./feeds/luci/collections/luci/Makefile
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# 修改想要的root密码
+#sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:password/g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
+#sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
+
+# 修改默认wifi名称ssid为Xiaomi_R3P
+#sed -i 's/ssid=OpenWrt/ssid=openwrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改默认wifi密码key为Xiaomi_R3P
+#sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#使用sed 在第四行后添加新字
+#sed -e 120a\set wireless.default_radio${devidx}.key=XiaoWanSM package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
 rm -f package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
